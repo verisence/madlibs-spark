7fe8d1dfcd2bb38e88db3c0478c1bbf262bf87e0
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-17 13:47:51 +0300

    ready for deployment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,16 @@ import  static spark.Spark.*;
 
 public class App {
     public static void main (String[] args){
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
+
         String layout = "templates/layout.vtl";
 
         get("/", (request, response) -> {
